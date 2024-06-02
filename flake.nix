868df5cc119b4fd6f630aebc742e20b5c9fcecd4
lukejcollins <vannharl@live.co.uk>
2024-06-02 09:37:41 +0100

    Neaten up and comment flake.nix

--- flake.nix
+++ flake.nix
@@ -1,12 +1,15 @@
 {
   inputs = {
+    # Importing the nixpkgs repository from GitHub with the nixos-unstable branch
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
+    # Importing the nixos-cosmic repository from GitHub and linking its nixpkgs input to the above nixpkgs
     nixos-cosmic = {
       url = "github:lilyinstarlight/nixos-cosmic";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # Importing the home-manager repository from GitHub and linking its nixpkgs input to the above nixpkgs
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -15,28 +18,35 @@
 
   outputs = { self, nixpkgs, nixos-cosmic, home-manager }: {
     nixosConfigurations = {
+      # Configuration for mySystem
       mySystem = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           {
+            # Nix settings for substituters and trusted-public-keys
             nix.settings = {
               substituters = [ "https://cosmic.cachix.org/" ];
               trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
             };
           }
+          # Including the default NixOS Cosmic module
           nixos-cosmic.nixosModules.default
+          # Including the user's configuration
           ./configuration.nix
+          # Including the home-manager NixOS module
           home-manager.nixosModules.home-manager
         ];
       };
     };
 
     homeConfigurations = {
+      # Configuration for user lukecollins
       lukecollins = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
         modules = [
           ./home.nix
           {
+            # State version and user-specific settings
             home.stateVersion = "24.05"; # Add the appropriate state version
             home.username = "lukecollins";
             home.homeDirectory = "/home/lukecollins";
